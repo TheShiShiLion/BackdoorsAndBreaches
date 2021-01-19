240ed3da0d4780dce36b59eff40442fd93ab99ad
puc4 <33991766+puc4@users.noreply.github.com>
2021-01-19 22:17:47 +0000

    Minor changes to the testHarness to aid with setup and familiarisation

--- testHarness.ps1
+++ testHarness.ps1
@@ -9,16 +9,22 @@ Import-Module .\BackdoorsAndBreaches\BackdoorsAndBreaches.psd1 -Force
 Get-Module -Name BackdoorsAndBreaches
 Clear-Host
 
+# Create directories for testing
+$MasterPath = "C:\temp\master\"
+If(!(test-path $MasterPath)) { New-Item -ItemType Directory -Force -Path $MasterPath }
+$PlayerPath = "C:\temp\player\"
+If(!(test-path $PlayerPath)) { New-Item -ItemType Directory -Force -Path $PlayerPath }
+
 # Vanilla Game on local network - no FTPS upload.  Ideal for playing on corporate network with PlayerOutputPath to share drive accessible by all players
-$game =Invoke-BackdoorsAndBreaches -Players "Alice,Bob,Carol,Dave,Eve" -IncidentMasterOutputPath "C:\temp\master\" -PlayerOutputPath "C:\temp\player\"
+$game =Invoke-BackdoorsAndBreaches -Players "Alice,Bob,Carol,Dave,Eve" -IncidentMasterOutputPath $MasterPath -PlayerOutputPath $PlayerPath
 
 # Game hosted on a remote server e.g. FTPS.  Obviously may not work on corporate networks where firewall rules don't allow chosen file tfer protocol acccess outbound.
 # Easier to play from off corporate network if connectivity issues are encountered.
-#$game =Invoke-BackdoorsAndBreaches -Players "Alice,Bob,Carol,Dave,Eve" -IncidentMasterOutputPath "C:\temp\master\" -PlayerOutputPath "C:\temp\player\" -FileXferConfig "C:\temp\fileXfer.config"
-
+# $game =Invoke-BackdoorsAndBreaches -Players "Alice,Bob,Carol,Dave,Eve" -IncidentMasterOutputPath $MasterPath -PlayerOutputPath $PlayerPath -FileXferConfig "C:\temp\fileXfer.config"
+#
 # IMPORTANT: navigate to the incidentmaster link in a browser and click on cards to see commands that can be run against the created $game object.
 # The incidnet master can use this as a cheatsheet to ensure the right command is run based upon the cards displayed.
-
+#
 # For testing purposes and to familiarise yourself with the gameplay you can step through the below in debug mode to see how gameplay proceeds on both
 # the player and incident master dashboards.
 
@@ -34,7 +40,7 @@ $game.PlayProcedure(2)
 # Play fourth prodedure
 $game.PlayProcedure(3)
 
-# Play a prodedure of the players choice (no card selected - no +3 modifier)
+# Play a prodedure of the players choice/invention/experience (no card selected - no +3 modifier)
 $game.TakeTurn($false)
 
 # Players solve Initial Compromise - reveal card
